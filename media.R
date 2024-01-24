d4b0d6ab5de4cfa0f1887f73f692d42a3d499dbb
Gabriel Monteiro Rodrigues de Oliveira <98244095+gabriel-monteiro-rodrigues-de-oliveira@users.noreply.github.com>
2024-01-24 17:46:53 -0300

    aula criada

---
+++ media.R
@@ -0,0 +1,36 @@
+## Exemplo
+
+#--- Dados
+library(datasets) # importar biblioteca 'datasets'
+                  # antes instalar a biblioteca com 'install.packages("datasets")'
+
+dados <- cars # atribuir a variavel 'dados' o conjunto de dados 'cars' de 'datasets'
+
+#--- Media
+media <- mean(cars$speed) # atribuir a variavel 'media' a media da coluna 'speed' do conjunto 'cars'
+
+#--- Moda
+library(DescTools) # importar biblioteca 'DescTools'
+
+moda_dist <- Mode(cars$dist)
+moda_speed <- Mode(cars$speed) # 'Mode()' e um metodo da biblioteca 'DescTools'
+
+#--- Mediana
+mediana <- median(cars$dist)
+
+#--- Desvio padrão
+
+desvio_padrao <- sd(cars$dist)
+
+#--- Correlação
+
+correlacao <- cor(cars$speed, cars$dist)
+
+correlacao <- cor.test(cars$speed, cars$dist)
+
+#--- Amostra
+
+amostra <- sample(1:50, 10, replace = FALSE)
+
+amostras <- dados[amostra, ]
+
